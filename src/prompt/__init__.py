514ab22a7437fe810e4e713ddf3dd0964035d35b
Stefan Fischer <sfischer13@ymail.com>
2015-09-15 22:07:24 +0200

    new: prompt.character new: prmopt.regex new: empty keyword parameter

--- src/prompt/__init__.py
+++ src/prompt/__init__.py
@@ -29,6 +29,8 @@ The package may be imported directly::
 The library was initiated by Stefan Fischer and is developed and maintained by many others.
 """
 
+import re
+
 __author__ = "Stefan Fischer"
 __contact__ = "Stefan Fischer <sfischer13@ymail.com>"
 __copyright__ = "Copyright (c) 2015 Stefan Fischer"
@@ -41,18 +43,63 @@ __version__ = "0.1.0a1"
 PROMPT = "? "
 
 
-def integer(prompt=PROMPT):
-    s = input(prompt)
-    i = int(s)
-    return i
+def character(prompt=None, empty=False):
+    s = _prompt_input(prompt)
+    if empty and not s:
+        return None
+    elif len(s) == 1:
+        return s
+    else:
+        return character(prompt=prompt, empty=empty)
 
 
-def real(prompt=PROMPT):
-    s = input(prompt)
-    r = float(s)
-    return r
+def integer(prompt=None, empty=False):
+    s = _prompt_input(prompt)
+    if empty and not s:
+        return None
+    else:
+        try:
+            return int(s)
+        except ValueError:
+            return integer(prompt=prompt, empty=empty)
 
 
-def string(prompt=PROMPT):
-    s = input(prompt)
-    return s
+def real(prompt=None, empty=False):
+    s = _prompt_input(prompt)
+    if empty and not s:
+        return None
+    else:
+        try:
+            return float(s)
+        except ValueError:
+            return real(prompt=prompt, empty=empty)
+
+
+def regex(pattern, prompt=None, empty=False, flags=0):
+    s = _prompt_input(prompt)
+    if empty and not s:
+        return None
+    else:
+        m = re.match(pattern, s, flags=flags)
+        if m:
+            return m
+        else:
+            return regex(pattern, prompt=prompt, empty=empty, flags=flags)
+
+
+def string(prompt=None, empty=False):
+    s = _prompt_input(prompt)
+    if empty and not s:
+        return None
+    else:
+        if s:
+            return s
+        else:
+            return string(prompt=prompt, empty=empty)
+
+
+def _prompt_input(prompt):
+    if prompt is None:
+        return input(PROMPT)
+    else:
+        return input(prompt)
