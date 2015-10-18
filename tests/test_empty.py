b022d756da7336e84ffaeb0b8287fc3c950ff084
Stefan Fischer <sfischer13@ymail.com>
2015-10-18 18:18:00 +0200

    Added some tests for empty responses.

---
+++ tests/test_empty.py
@@ -0,0 +1,36 @@
+import builtins
+import getpass
+
+import pytest
+
+import prompt
+
+
+@pytest.fixture(autouse=True)
+def input_empty(monkeypatch):
+    monkeypatch.setattr(builtins, "input", lambda prompt: "")
+    monkeypatch.setattr(getpass, "getpass", lambda prompt: "")
+
+
+def test_character():
+    assert prompt.character(empty=True) is None
+
+
+def test_integer():
+    assert prompt.integer(empty=True) is None
+
+
+def test_real():
+    assert prompt.real(empty=True) is None
+
+
+def test_regex():
+    assert prompt.regex("foo", empty=True) is None
+
+
+def test_secret():
+    assert prompt.secret(empty=True) is None
+
+
+def test_string():
+    assert prompt.string(empty=True) is None
