591e514d44a2bd1fdaafb5cf1f9ad338333ef136
Stefan Fischer <sfischer13@ymail.com>
2017-06-05 20:16:07 +0200

    Use numpydoc

--- docs/conf.py
+++ docs/conf.py
@@ -33,8 +33,8 @@ sys.path.insert(0, os.path.abspath('../'))
 # ones.
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.coverage',
-    'sphinx.ext.viewcode'
-]
+    'sphinx.ext.viewcode',
+    'numpydoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -155,3 +155,6 @@ texinfo_documents = [
      author, 'prompt', 'One line description of project.',
      'Miscellaneous'),
 ]
+
+
+
